c7d17bf3445c607fcb1901bcf30e0ffdb067820e
tom2002965 <tom2002965@gmail.com>
2017-10-29 16:23:23 +0800

    Day 8

---
+++ Day_8_BinarySearch.py
@@ -0,0 +1,16 @@
+def search(data, item):
+    left, right = 0, len(data)-1
+    
+    while left <= right:
+        middle = ( left + right ) // 2
+        
+        if item < data[middle]:
+            right = middle - 1 
+        elif item > data[middle]:
+            left = middle + 1
+        else:
+            return middle
+    return -1
+
+
+print search([2, 3, 4, 8, 10], 8)
